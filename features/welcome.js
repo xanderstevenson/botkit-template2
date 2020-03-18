3481840661221dac9200d4ac09eaec9cd96a2031
Alexander W. Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2020-03-18 10:22:11 -0500

    Update welcome.js
    Adding links to list of options, trying anchor tag

--- features/welcome.js
+++ features/welcome.js
@@ -9,7 +9,11 @@ module.exports = function (controller) {
         let markDown = `## **Welcome to DevNet!**\n\n Hi, I'm the **${ controller.adapter.identity.displayName }**!  \
                         \n\n If you need to submit a ticket, you can do so here \
                         --> https://devnetsupport.cisco.com/hc/en-us/requests/new \
-                        \n\n Here are some options for Self-Help: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n`
+                        \n\n Here are some options for Self-Help:\
+                        \n 1. https://developer.cisco.com/docs/fundamentals/help/ <a href='https://developer.cisco.com/docs/fundamentals/help/'>FAQs<a> \
+                        \n 2. Option 2 \
+                        \n 3. Option 3 \
+                        \n`
 
 
         if ( message.data.roomType == 'group' ) {
