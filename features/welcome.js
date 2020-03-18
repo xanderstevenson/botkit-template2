33e501079c304a7ccea0f4ddc1f62fd5cee461cf
Alexander W. Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2020-03-18 10:08:09 -0500

    Update welcome.js
    adjusted lines ending in \n to \

--- features/welcome.js
+++ features/welcome.js
@@ -7,8 +7,9 @@ module.exports = function (controller) {
     controller.on( 'memberships.created', async( bot, message ) => {
 
         let markDown = `## **Welcome to DevNet!**\n\n Hi, I'm the **${ controller.adapter.identity.displayName }**!  \
-                        \n\n If you need to submit a ticket, you can do so here --> https://devnetsupport.cisco.com/hc/en-us/requests/new \n \
-                        Here are some options for Self-Help: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n`
+                        \n\n If you need to submit a ticket, you can do so here \
+                        --> https://devnetsupport.cisco.com/hc/en-us/requests/new \
+                        \n\n Here are some options for Self-Help: \n 1. Option 1 \n 2. Option 2 \n 3. Option 3 \n`
 
 
         if ( message.data.roomType == 'group' ) {
