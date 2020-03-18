cdc63192d86c510862194c8a124f53e1dfc967b9
Alexander W. Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2020-03-18 12:02:58 -0500

    Update welcome.js
    Fixing up the links and adding strong tags

--- features/welcome.js
+++ features/welcome.js
@@ -7,23 +7,18 @@ module.exports = function (controller) {
     controller.on( 'memberships.created', async( bot, message ) => {
 
         let markDown = `## **Welcome to DevNet!**\n\n Hi, I'm the **${ controller.adapter.identity.displayName }**!  \
-                        \n\n If you need to submit a ticket, you can do so here \
-                        --> https://devnetsupport.cisco.com/hc/en-us/requests/new \
                         \n\n Your Self-Help Options for the DevNet Associate Fundamentals Course:\
-                        \n 1. Check the course <a href='https://developer.cisco.com/docs/fundamentals/help/'>FAQs<a> \
-                        \n 2. For any questions on content, or for help with the course labs, please go to the <a href='https://learningnetwork.cisco.com/s/topic/0TO3i0000008jY5GAI/devnet-certifications-community'> \
-                            DevNet Certifications Community</a> and ask/interact with the Experts there. \
-                        \n 3. For any <strong>technical issues</strong> with your course, including page appearance, video or lab functionality, or payments, please open a ticket with \
-                        <a href='https://devnetsupport.cisco.com/hc/en-us/requests/new?ticket_form_id=360002862214'>DevNet Course Support</a> \
-//                         \n\t\t\t e.g. The Terminal isn't reciving input \
-//                         \n\t\t\t e.g. \
-//                         \n\t\t\t Content \
+                        \n 1. <strong>Search for common issues and their answers</strong> in the course <a href='https://developer.cisco.com/docs/fundamentals/help/'>FAQs<a> \
+                        \n 2. For any <strong>questions on content</strong>, or for <strong>help with the course labs</strong>, please go to the DevNet Certifications Community and<a href='https://learningnetwork.cisco.com/s/topic/0TO3i0000008jY5GAI/devnet-certifications-community'> \
+                             ask the Experts</a> there. \
+                        \n 3. For any <strong>technical issues</strong> with your course, including page appearance, video or lab functionality, or payments, please \
+                        <a href='https://devnetsupport.cisco.com/hc/en-us/requests/new?ticket_form_id=360002862214'>open a ticket</a> with DevNet Course Support \
                         \n`
 
 
         if ( message.data.roomType == 'group' ) {
 
-            markDown += `\n_Note that this is a "group" space.\n_I will answer only if mentioned!  \n`
+            markDown += `\n_Note_ this is a "group" space.\n_I will answer only if mentioned!  \n`
             markDown += `For help, enter: ${ controller.checkAddMention( message.data.roomType, 'help' ) }`
         }
         console.log('memberships created', message);
