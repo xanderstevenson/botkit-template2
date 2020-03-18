b65c552b857256264263544ec0a9e8e7a1517631
Alexander W. Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2020-03-18 14:01:59 -0500

    Update welcome.js
    various markdown changes

--- features/welcome.js
+++ features/welcome.js
@@ -7,12 +7,12 @@ module.exports = function (controller) {
     controller.on( 'memberships.created', async( bot, message ) => {
 
         let markDown = `## **Welcome to DevNet!**\
-                        \n\n![alt text](https://en.wikipedia.org/wiki/Cisco_DevNet#/media/File:CiscoDevNet2.png "DevNet")
+                        \n\n![alt text](https://github.com/xanderstevenson/botkit-template/blob/master/assets/images/CiscoDevNetSmall.png "DevNet")
                         \n\n Hi, I'm the **${ controller.adapter.identity.displayName }**!  \
                         \n\n Here are your Self-Help Options for the DevNet Associate Fundamentals Course:\
                         \n --- \
-                        \n * <u>Search for common issues and their answers</u> in the course <a href='https://developer.cisco.com/docs/fundamentals/help/'>FAQs<a>. \
-                        \n * For any <span style="color:#009edc">questions on content</span>, or for <strong>help with the course labs</strong>, please go to the DevNet Certifications Community and<a href='https://learningnetwork.cisco.com/s/topic/0TO3i0000008jY5GAI/devnet-certifications-community'> \
+                        \n * *Search for common issues and their answers* in the course <a href='https://developer.cisco.com/docs/fundamentals/help/'>FAQs<a>. \
+                        \n * For any <span style="color:blue;">questions on content</span>, or for <strong>help with the course labs</strong>, please go to the DevNet Certifications Community and<a href='https://learningnetwork.cisco.com/s/topic/0TO3i0000008jY5GAI/devnet-certifications-community'> \
                              ask the Experts</a> there. \
                         \n * For any **technical issues** with your course, including page appearance, video or lab functionality, or payments, please \
                         <a href='https://devnetsupport.cisco.com/hc/en-us/requests/new?ticket_form_id=360002862214'>open a ticket</a> with DevNet Course Support \
@@ -21,7 +21,7 @@ module.exports = function (controller) {
 
         if ( message.data.roomType == 'group' ) {
 
-            markDown += `\n_Note_ this is a "group" space.\n and I (the Bot) will answer only if mentioned!  \n`
+            markDown += `\n_<u>Note</u>:_ this is a "group" space and I (the Bot) will answer only if mentioned!  \n`
             markDown += `For help, enter: ${ controller.checkAddMention( message.data.roomType, '' ) } help`
         }
         console.log('memberships created', message);
